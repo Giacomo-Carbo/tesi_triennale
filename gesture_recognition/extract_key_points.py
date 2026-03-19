d10242a00d4ede8c3d1c7ba567a2e714cd08ae37
Giacomo Carbonara <giacomocarbo175@icloud.com>
2026-03-19 13:04:08 +0100

    first commit

---
+++ gesture_recognition/extract_key_points.py
@@ -0,0 +1,9 @@
+import kagglehub
+#import cv2
+#import mediapipe as mp
+#import numpy as np
+#import json
+
+# Download del dataset
+datset_path = kagglehub.dataset_download("risangbaskoro/wlasl-processed")
+video_path = datset_path + "/videos"
