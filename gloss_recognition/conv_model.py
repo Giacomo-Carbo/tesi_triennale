92fdfb1c7e43c4d51447692de66b2ad3079119ce
Giacomo Carbonara <giacomocarbo175@icloud.com>
2026-05-07 17:16:33 +0200

    Remove unused TensorFlow.js model export code from conv_model.py

--- gloss_recognition/conv_model.py
+++
@@ -1,8 +0,0 @@
-import tensorflowjs as tfjs
-from tensorflow import keras
-
-# carico il modello 
-model = keras.models.load_model("final_lstm_encoder.h5", compile=False)
-
-# esporta in formato TensorFlow.js
-tfjs.converters.save_keras_model(model, "modeljs")
